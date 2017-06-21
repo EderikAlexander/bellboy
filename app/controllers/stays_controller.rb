2c6bacd238e4d19310a1857cc43bb5d7d7e501da
Albert Montolio <albert.montolio@gmail.com>
2017-06-21 17:59:25 +0200

    index landing page

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -1,10 +1,9 @@
 class StaysController < ApplicationController
 
 def index
-
   # STAY OF THE CURRENT USER
-
-  @stays = Stay.where(current_user)
+  # @stays = Stay.where(current_user)
+  @stays = Stay.all
 end
 
 def show
