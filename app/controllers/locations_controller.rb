a96ebc428b81d97f2fe13128dc687833c3fe2ebc
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2017-06-22 00:55:48 +0200

    Add map on locations and snazzy map style

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,18 @@ class LocationsController < ApplicationController
   def index
     @stay = Stay.find(params[:stay_id])
     @hotel = Hotel.find(params[:hotel_id])
+    # @locations = Location.all
+
     @locations = Location.all
+    @locations = Location.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
+
+
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621134220) do
+ActiveRecord::Schema.define(version: 20170621215729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170621134220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["deleted_at"], name: "index_locations_on_deleted_at", using: :btree
     t.index ["hotel_id"], name: "index_locations_on_hotel_id", using: :btree
   end
