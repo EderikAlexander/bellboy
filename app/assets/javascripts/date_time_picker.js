7df7f1aa4984052cc84a01aea77f7659205b0479
Albert Montolio <albert.montolio@gmail.com>
2017-06-23 14:59:38 +0200

    done

---
+++ app/assets/javascripts/date_time_picker.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#start_datetime').datetimepicker(
+      //defaultDate: "11/1/2013",
+
+      // disabledDates:
+
+    );
+  $('#end_datetime').datetimepicker();
+});
+
+
