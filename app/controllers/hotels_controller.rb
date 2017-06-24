497ca60f7e0ef3f354bb299a55ab59fd0579d33d
Albert Montolio <albert.montolio@gmail.com>
2017-06-24 15:45:34 +0200

    filter for calendar admin

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -6,35 +6,42 @@ class HotelsController < ApplicationController
   end
 
   def calendar_month
-    @stay = Stay.find(params[:stay_id])
-    @hotel = Hotel.find(params[:hotel_id])
-
-    @hotel_bookings = []
-    @hotel.services.each do |service|
-      service.bookings.each do |booking|
-        @hotel_bookings << booking
-      end
-    end
-
-    @start_date = params.fetch(:start_date, Date.today).to_date
+    calendar_info_generation
     @date_range = (@start_date.beginning_of_month.beginning_of_week..@start_date.end_of_month.end_of_week).to_a
   end
 
   def calendar_week
+    calendar_info_generation
+    #calculation for week calendar
+    @date_range = (@start_date.beginning_of_week..@start_date.end_of_week).to_a
+  end
+
+
+
+  def calendar_info_generation
     @stay = Stay.find(params[:stay_id])
     @hotel = Hotel.find(params[:hotel_id])
 
-    # bookings for all the services of an hotel
     @hotel_bookings = []
+
     @hotel.services.each do |service|
       service.bookings.each do |booking|
         @hotel_bookings << booking
       end
     end
 
+    @restaurants_id = @hotel.services[0].id
+    @massages_id = @hotel.services[1].id
+
+    if params[:filter] == "restaurants"
+      @hotel_bookings = @hotel_bookings.select { |hotel_booking| hotel_booking.service_id == @restaurants_id }
+    end
+
+    if params[:filter] == "massages"
+      @hotel_bookings = @hotel_bookings.select { |hotel_booking| hotel_booking.service_id == @massages_id }
+    end
+
     @start_date = params.fetch(:start_date, Date.today).to_date
-    #calculation for week calendar
-    @date_range = (@start_date.beginning_of_week..@start_date.end_of_week).to_a
   end
 
 end
