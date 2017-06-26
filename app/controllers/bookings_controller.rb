52c50be15bab42500f2b56776885897300a602f9
Albert Montolio <albert.montolio@gmail.com>
2017-06-26 17:14:46 +0200

    finish debuggint

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -27,7 +27,8 @@ class BookingsController < ApplicationController
     # end_datetime_string = booking_params["end_datetime"]
     # end_datetime_tobook = DateTime.strptime(end_datetime_string, "%m/%d/%Y %H:%M %P")
 
-    start_date_hour_string = params["time_booking"]["day_selection"]
+    start_date_hour_string = booking_params["day_selection"]
+    # start_date_hour_string = params["time_booking"]["day_selection"]
     #=> "2017-06-15|11"
     start_date_string = start_date_hour_string.split("|")[0]
     #=> "2017-06-15"
@@ -40,9 +41,9 @@ class BookingsController < ApplicationController
     day = start_date_time.day
 
     start_datetime_tobook = DateTime.new(year, month, day, hour, 0, 0, '+2')
-    raise
+    end_datetime_tobook = DateTime.new(year, month, day, hour, 59, 0, '+2')
 
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new()
     @booking.start_datetime = start_datetime_tobook
     @booking.end_datetime = end_datetime_tobook
 
@@ -90,7 +91,7 @@ class BookingsController < ApplicationController
 
   private
   def booking_params
-    params.require(:booking).permit(:start_datetime, :end_datetime)
+    params.require(:time_booking).permit(:day_selection)
     # params.require(:booking).permit(:start_datetime, :end_datetime)
   end
 
